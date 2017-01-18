91f9874d57858475b6bb391f6457ca9f948f8450
Aaren Tebbutt <aaren.tebbutt@noshjournal.com>
2017-01-19 00:19:19 +1100

    Implemented file reading and parsing

--- index.js
+++ index.js
@@ -2,3 +2,31 @@
 
 let parse = require('csv-parse'),
     fs = require('fs');
+
+fs.readFile('sample.csv', 'utf8', (fileReadErr, data) => {
+    if(fileReadErr) {
+        console.log('Error: Unable to read sample.csv');
+        console.log(fileReadErr.message);
+        return;
+    }
+
+    parse(data, {columns: true, trim: true}, (parseErr, records) => {
+        if(parseErr){
+            console.log('Error: Unable to parse sample.csv');
+            console.log(parseErr.message);
+            return;
+        }
+
+        records.forEach((record) => {
+
+            if(record.Name) {
+                console.log(record.Name);
+            } else {
+                console.log('Error: Name column missing from sample data');
+                return;
+            }
+
+        });
+
+    });
+});
